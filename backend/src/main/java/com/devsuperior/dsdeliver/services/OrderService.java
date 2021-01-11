b644d3c9de8fdb5bac6a7985a3433fec18e3d012
root <!gown119>
2021-01-11 10:17:51 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,9 +1,26 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.repository.OrderRepository;
 
 @Service
 public class OrderService {
 
+	@Autowired
+	private OrderRepository repository;
 	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		
+		return this.repository.findOrdersWithProducts().stream().
+				map(p-> new OrderDTO(p)).
+				collect(Collectors.toList());
+	}
 }
